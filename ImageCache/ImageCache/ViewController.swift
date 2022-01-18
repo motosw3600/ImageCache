252f75d2d49df0c7f8127a5a74ba21c3177942d5
motosw3600 <motosw3600@gmail.com>
2022-01-18 14:50:14 +0900

    feat: ImageView request

--- ImageCache/ImageCache/ViewController.swift
+++ ImageCache/ImageCache/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.requestImage()
     }
-
-
+    
+    private func requestImage() {
+        let imageUrl = "https://img3.yna.co.kr/photo/cms/2019/04/09/12/PCM20190409000012005_P2.jpg"
+        ImageCacheManager.shared.loadImage(url: URL(string: imageUrl)) { data in
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+    }
+    
 }
 
